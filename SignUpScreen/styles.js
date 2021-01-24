a88b80eff90c5ccaf50c6993d6c6faa148cac1ec
Help <pczerokey@gmail.com>
2021-01-24 02:20:19 -0500

    Styles for creating account page

--- SignUpScreen/styles.js
+++ SignUpScreen/styles.js
@@ -1,5 +1,27 @@
-import {Platform, StatusBar, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import colors from '../colorScheme';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    //Setting font type works different
+    // fontFamily: 'Poppins',
+    fontSize: 20,
+  },
+  textInput: {
+    borderBottomColor: '#000000',
+    borderBottomWidth: 1,
+  },
+  button: {
+    backgroundColor: colors.blue,
+    color: colors.white,
+    padding: 15,
+    margin: 10,
+    width: 300,
+    borderRadius: 15,
+  },
+  buttonText: {
+    color: colors.white,
+    textAlign: 'center',
+  },
+});
 export default styles;
