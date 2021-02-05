475bd831442ed9b69d85f57384b56c49a30af502
Help <pczerokey@gmail.com>
2021-02-05 01:14:30 -0500

    add firebase config

--- App.js
+++ App.js
@@ -10,6 +10,20 @@ import React from 'react';
 
 import LandingScreen from './LandingScreen/LandingScreen';
 import SignUpScreen from './SignUpScreen/SignUpScreen';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyBJpla0_J-bleeJFWn6lUidRUAbS-S8WP0',
+  authDomain: 'afterhoursreactnative.firebaseapp.com',
+  projectId: 'afterhoursreactnative',
+  storageBucket: 'afterhoursreactnative.appspot.com',
+  messagingSenderId: '37657935947',
+  appId: '1:37657935947:web:4849440ea5532f8ca7cdce',
+  measurementId: 'G-BGJQRRL9JZ',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 const App: () => React$Node = () => {
   return (
