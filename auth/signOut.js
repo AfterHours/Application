97af907b16030afea6db44b4ab5eafe8501cdb72
Help <pczerokey@gmail.com>
2021-02-10 19:52:39 -0500

    add error logging

--- auth/signOut.js
+++ auth/signOut.js
@@ -1,7 +1,11 @@
 import auth from '@react-native-firebase/auth';
 
-export const signOut = () => {
+export const signOut = async () => {
   auth()
     .signOut()
-    .then(() => console.log('User signed out!'));
+    .then(() => console.log('User signed out!'))
+    .catch((error) => {
+      console.error(error);
+      throw new Error('Error while signing out!');
+    });
 };
