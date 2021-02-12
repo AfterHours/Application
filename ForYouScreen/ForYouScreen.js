8ac4132b06ddd5709ce49e3544d741a865272b89
Help <pczerokey@gmail.com>
2021-02-12 01:30:29 -0500

    Created For You Screen(Temporary)

---
+++ ForYouScreen/ForYouScreen.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text, Image} from 'react-native';
+
+import styles from './styles';
+import SignUpButton from '../components/SignUpButton';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+const LandingScreen: () => React$Node = ({navigation}) => {
+  return (
+    <SafeAreaView>
+      <Text>For You</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LandingScreen;
