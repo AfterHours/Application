7a951384603402571f897f1c2813d0847adf4383
Help <pczerokey@gmail.com>
2021-02-17 22:59:33 -0500

    added returns for identifying login attempts

--- auth/signIn.js
+++ auth/signIn.js
@@ -5,13 +5,16 @@ export const signIn = async (email, password) => {
     .signInWithEmailAndPassword(email, password)
     .then(() => {
       console.log('User account signed in!');
+      return true;
     })
     .catch((error) => {
       console.error(error);
       if (error.code === 'auth/invalid-email') {
         console.log('That email address is invalid!');
+        return false;
       } else {
         console.log('Generic error with sign in');
       }
+      return 'error';
     });
 };
