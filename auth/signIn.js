a7f10d8a29b3076fd7581fda7850cbcaab9e25c3
Help <pczerokey@gmail.com>
2021-02-19 21:40:22 -0500

    different return logic

--- auth/signIn.js
+++ auth/signIn.js
@@ -1,21 +1,23 @@
 import auth from '@react-native-firebase/auth';
 
 export const signIn = async (email, password) => {
+  let code;
   await auth()
     .signInWithEmailAndPassword(email, password)
     .then(() => {
       console.log('User account signed in!');
-      return true;
+      code = true;
     })
     .catch((error) => {
       console.error(error);
       if (error.code === 'auth/invalid-email') {
         console.log('The email address or password is invalid!');
-        return false;
+        code = false;
       } else {
         //TODO WHY DOES THIS NOT RUN ON ERROR
         console.log('Error with sign in', error);
       }
-      return 'error';
+      code = 'error';
     });
+  return code;
 };
