33d1034b4cb64bcafb20b18b7949cf813173783f
Help <pczerokey@gmail.com>
2021-02-19 22:43:48 -0500

    enhanced register logic

--- auth/registerEmail.js
+++ auth/registerEmail.js
@@ -1,20 +1,19 @@
 import auth from '@react-native-firebase/auth';
 
 export const registerEmail = async (email, password) => {
-  auth()
+  return await auth()
     .createUserWithEmailAndPassword(email, password)
     .then(() => {
       console.log('User account created & signed in!');
+      return true;
     })
     .catch((error) => {
+      console.error(error);
       if (error.code === 'auth/email-already-in-use') {
         console.log('That email address is already in use!');
-      }
-
-      if (error.code === 'auth/invalid-email') {
+      } else if (error.code === 'auth/invalid-email') {
         console.log('That email address is invalid!');
       }
-
-      console.error(error);
+      return false;
     });
 };
